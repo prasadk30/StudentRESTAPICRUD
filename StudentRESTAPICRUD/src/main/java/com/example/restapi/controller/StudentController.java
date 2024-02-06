283ff0d6094bcf8507c50a1f7c1fa743169f2db1
prasa <prasadkumbhar9565@gmail.com>
2024-02-06 16:31:13 +0530

    Initial Commit.

---
+++ StudentRESTAPICRUD/src/main/java/com/example/restapi/controller/StudentController.java
@@ -0,0 +1,67 @@
+package com.example.restapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.restapi.entity.Student;
+import com.example.restapi.repository.StudentRepository;
+
+@RestController
+public class StudentController {
+	
+	@Autowired
+	StudentRepository studentRepository;
+	
+	// get all the students list
+	//write URI for get all students
+	@GetMapping("/students")
+	public List<Student> getAllStudents(){
+		
+		List<Student> students= studentRepository.findAll();
+		
+		return students;
+	}
+	
+	// uri is (localhost:8080/students/1)
+	@GetMapping("/students/{id}")
+	public Student getStudent(@PathVariable int id) {
+
+		Student student=studentRepository.findById(id).get();
+		
+		return student;
+	}
+	
+	@PostMapping("/student/add")  // create new student
+	@ResponseStatus(code = HttpStatus.CREATED) // for show the status code 201 at the Postman
+	public void createStudent(@RequestBody Student student) {
+		studentRepository.save(student);
+	}
+	
+	@PutMapping("/student/update/{id}") //update the student
+	public Student updateStudents(@PathVariable int id) {  // return the updated student details
+		Student student=studentRepository.findById(id).get();
+		student.setName("poonam");
+		student.setPercentage(92);
+		
+		studentRepository.save(student);
+		
+		return student;
+	}
+	
+	@DeleteMapping("/student/delete/{id}")
+	public void deleteStudent(@PathVariable int id) {
+		Student student= studentRepository.findById(id).get();
+		studentRepository.delete(student);
+		
+	}
+}
---
+++ StudentRESTAPICRUD/src/main/java/com/example/restapi/entity/Student.java
@@ -0,0 +1,68 @@
+package com.example.restapi.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int rollNo;
+	@Column(name = "stud_name")
+	private String name;
+	@Column(name = "stud_percentage")
+	private float percentage;
+	@Column(name = "stud_branch")
+	private String branch;
+	
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getPercentage() {
+		return percentage;
+	}
+	public void setPercentage(float percentage) {
+		this.percentage = percentage;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	@Override
+	public String toString() {
+		return "Student [rollNo=" + rollNo + ", name=" + name + ", percentage=" + percentage + ", branch=" + branch
+				+ "]";
+	}
+
+	public Student(String name, float percentage, String branch) {
+		super();
+		this.name = name;
+		this.percentage = percentage;
+		this.branch = branch;
+	}
+	
+	
+	
+
+}
